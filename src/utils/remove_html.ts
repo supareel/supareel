dbc07bcf53ade89ec4f6f72f1c0a0aa7cf80c836
sourabhmandal <19mandal97@gmail.com>
2024-02-01 16:19:26 +0530

    fix: remove html tags

--- src/utils/remove_html.ts
+++ src/utils/remove_html.ts
@@ -1,4 +1,4 @@
 export function removeHtmlTags(inputString: string): string {
   // Use a regular expression to remove HTML tags and content
-  return inputString.replace(/<[^>]*>/g, "");
+  return inputString.replace(/<|>/g, "").replace(/\.\.\//g, "");
 }
